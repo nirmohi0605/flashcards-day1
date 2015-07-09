014d44948217748336fbe3ec0fe2efe01b635429
Nirmohi <nirmohi0605@gmail.com>
2015-07-09 12:34:57 -0400

    done with the workshop

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 
+
 var app = express(); // Create an express app!
 module.exports = app; // Export it so it can be require('')'d
 
@@ -22,6 +23,6 @@ var indexHtmlPath = path.join(__dirname, '../index.html');
 app.use(express.static(publicPath));
 
 // If we're hitting our home page, serve up our index.html file!
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(indexHtmlPath);
 });
